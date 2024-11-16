778cefa68a6a1722333b0bf62b6462280a5fafe2
GenesisG98 <gygp.dev@gmail.com>
2024-11-15 20:46:22 -0400

    Add files via upload

---
+++ js/botanimation.js
@@ -0,0 +1,255 @@
+window.sr = ScrollReveal();
+
+
+// Pag Principal
+
+    sr.reveal('.txt-principal', {
+        duration: 1500,
+        origin: 'right',
+        distance: '50px'
+    });
+    sr.reveal('.btn-1', {
+        duration: 800,
+        origin: 'right',
+        distance: '50px',
+        delay: 700,
+        scale: .9,
+    });
+
+    sr.reveal('.tlf', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 200,
+    });
+    sr.reveal('.telegram', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 400,
+    });
+    sr.reveal('.whatsapp', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 600,
+    });
+    sr.reveal('.sms', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 800,
+    });
+    sr.reveal('.total_bot', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1000,
+    });
+    sr.reveal('.bot', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1000,
+    });
+
+
+    //Pag2
+    sr.reveal('.tittle', {
+        duration: 2000,
+        origin: 'top',
+        distance: '25px',
+    });
+    sr.reveal('.totalbot1', {
+        duration: 2000,
+        origin: 'top',
+        distance: '25px',
+    });
+
+    sr.reveal('.container-txt-1', {
+        duration: 1500,
+        origin: 'right',
+        distance: '50px'
+    });
+    sr.reveal('.circle', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        rotate: {
+            z: -180,
+        },
+        delay: 200,
+    });
+    sr.reveal('.control', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 400,
+    });
+    sr.reveal('.circle1', {
+        duration: 600,
+        origin: 'left',
+        distance: '50px',
+        delay: 600,
+        rotate: {
+            z: -130,
+        },
+    });
+    sr.reveal('.IA', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 1000,
+    });
+    sr.reveal('.circle2', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        rotate: {
+            z: -130,
+        },
+        delay: 1000,
+    });
+    sr.reveal('.A-bot', {
+        duration: 500,
+        origin: 'left',
+        distance: '50px',
+        delay: 1400,
+    });
+    sr.reveal('.circle3', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        scale: .1,
+        rotate: {
+            z: -130,
+        },
+        delay: 1600,
+    });
+
+
+    
+    sr.reveal('.container-txt-3', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 500,
+    });
+    sr.reveal('.r-1', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 700,
+    });
+    sr.reveal('.r-2', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 800,
+    });
+    sr.reveal('.r-3', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 900,
+    });
+    sr.reveal('.r-4', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1000,
+    });
+    sr.reveal('.r-5', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1100,
+    });
+    sr.reveal('.r-6', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1200,
+    });
+    sr.reveal('.r-7', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1300,
+    });
+
+
+
+
+
+
+
+
+
+
+
+    // Pag4
+    sr.reveal('.container-txt-4', {
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        delay: 200,
+    });
+    sr.reveal('.tlf1', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 200,
+    });
+    sr.reveal('.sms11', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 800,
+    });
+    sr.reveal('.sms2', {
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        delay: 1400,
+    });
+    sr.reveal('.sms-2', {
+        duration: 1000,
+        scale: 1.2,
+        delay: 1800,
+    });
+    sr.reveal('.sms3', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 3000,
+    });
+    sr.reveal('.sms4', {
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        delay: 3600,
+    });
+    sr.reveal('.sms-4', {
+        duration: 1000,
+        scale: 1.2,
+        delay: 4000,
+    });
+    sr.reveal('.sms5', {
+        duration: 1000,
+        origin: 'left',
+        distance: '50px',
+        delay: 5200,
+    });
+    sr.reveal('.sms6', {
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        delay: 5800,
+    });
+    sr.reveal('.sms-6', {
+        duration: 1000,
+        origin: 'right',
+        distance: '50px',
+        delay: 6200,
+    });
